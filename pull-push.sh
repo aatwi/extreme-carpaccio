ff80de8d795ab7206c414d0b8f634f08e0bec6ed
Damien Menanteau <damien.menanteau@gmail.com>
2020-08-27 14:45:20 +0200

    Make git push global

--- pull-push.sh
+++ pull-push.sh
@@ -7,6 +7,6 @@
 export CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2)
 
 git pull --no-recurse-submodules origin "$CURRENT_BRANCH"
-git add katas
+git add .
 git commit -m 'Working...'
 git push --no-recurse-submodules origin "$CURRENT_BRANCH"
