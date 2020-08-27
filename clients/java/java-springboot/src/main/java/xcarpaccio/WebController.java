b0bb80c196e887a75b0b735651ef1e913871df79
Philippe Bourgau <philippe.bourgau@murex.com>
2020-08-27 15:40:55 +0200

    Working...

--- clients/java/java-springboot/src/main/java/xcarpaccio/WebController.java
+++ clients/java/java-springboot/src/main/java/xcarpaccio/WebController.java
@@ -41,7 +41,11 @@ public class WebController {
 
     Double computeAmount(Order order) {
         if (order.reduction.equals("STANDARD")) {
-            return order.getTotalAmount() * TaxRateHelper.getTaxRateFor(order.country);
+            double result = order.getTotalAmount() * TaxRateHelper.getTaxRateFor(order.country);
+            if (1000 <= result) {
+                throw new UnsupportedOperationException("We cannot yet handle reduction for more than 1000 amount");
+            }
+            return result;
         }
         throw new UnsupportedOperationException("The order reduction is not supported!");
     }
