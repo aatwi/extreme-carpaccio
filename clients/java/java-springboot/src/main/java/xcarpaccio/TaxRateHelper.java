50c0964af23c1c270a76584ed4637db7cf44905a
Philippe Bourgau <philippe.bourgau@murex.com>
2020-09-03 15:11:02 +0200

    Working...

--- clients/java/java-springboot/src/main/java/xcarpaccio/TaxRateHelper.java
+++ clients/java/java-springboot/src/main/java/xcarpaccio/TaxRateHelper.java
@@ -14,16 +14,17 @@ final class TaxRateHelper {
     }};
 
     public static double taxRateFor(String country) {
-
-        if (country.equals("HU")) {
-            return 1.27;
-        }
-        if (country.equals("DE")) {
-            return 1.20;
-        }
-        if (country.equals("PL")) {
-            return 1.21;
-        }
-        throw new UnsupportedOperationException("Tax calculation for: " + country + " is not supported!");
+        return taxPerCountry.get(country);
+//
+//        if (country.equals("HU")) {
+//            return 1.27;
+//        }
+//        if (country.equals("DE")) {
+//            return 1.20;
+//        }
+//        if (country.equals("PL")) {
+//            return 1.21;
+//        }
+//        throw new UnsupportedOperationException("Tax calculation for: " + country + " is not supported!");
     }
 }
