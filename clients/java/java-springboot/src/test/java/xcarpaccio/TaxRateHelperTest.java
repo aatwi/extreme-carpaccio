bab488755f816ffaa9bccd38713e6bdb66478ce1
Damien Menanteau <damien.menanteau@gmail.com>
2020-09-03 14:59:31 +0200

    Working...

--- clients/java/java-springboot/src/test/java/xcarpaccio/TaxRateHelperTest.java
+++ clients/java/java-springboot/src/test/java/xcarpaccio/TaxRateHelperTest.java
@@ -19,4 +19,9 @@ public class TaxRateHelperTest {
     public void it_should_apply_20_percent_tax_for_germany() {
         Assert.assertEquals(1.20, TaxRateHelper.taxRateFor("DE"), 0.0);
     }
+
+    @Test
+    public void it_should_apply_21_percent_tax_for_poland() {
+        Assert.assertEquals(1.21, TaxRateHelper.taxRateFor("PL"), 0.0);
+    }
 }
